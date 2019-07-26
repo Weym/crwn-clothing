168933aca64ced8283ea379b7fca869433f5a528
Weydson Marinho <weydsonmarinho@gmail.com>
2019-07-26 13:06:47 -0300

    added redux and implemented userReducer and userActions to header and components

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,6 +1,10 @@
 const INITIAL_STATE = {
   currentUser: null
 };
+<<<<<<< HEAD
+=======
+
+>>>>>>> 23aa576... added redux, and implemented userReducer and userActions to header and app components
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
