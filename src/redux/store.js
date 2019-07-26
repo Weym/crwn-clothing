931facde84e6667553cd6912e3b3f193d0092e5d
Weydson Marinho <weydsonmarinho@gmail.com>
2019-07-26 13:45:23 -0300

    quick fix related to the last push

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,10 +5,6 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-<<<<<<< HEAD
-const store = createStore(applyMiddleware(...middlewares));
-=======
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
->>>>>>> 23aa576... added redux, and implemented userReducer and userActions to header and app components
 
 export default store;
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,6 @@
 const INITIAL_STATE = {
   currentUser: null
 };
-<<<<<<< HEAD
-=======
-
->>>>>>> 23aa576... added redux, and implemented userReducer and userActions to header and app components
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
